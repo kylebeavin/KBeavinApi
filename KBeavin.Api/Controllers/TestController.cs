d487ee17ac3740e092019e9f484cc89725a58599
kyle beavin <kylebeavin@gmail.com>
2020-10-29 08:57:11 -0400

    Adding endpoints

--- KBeavin.Api/Controllers/TestController.cs
+++ KBeavin.Api/Controllers/TestController.cs
@@ -16,22 +16,36 @@ namespace KBeavin.Api.Controllers
         public IEnumerable<Test> Get()
         {
             List<Test> test = new List<Test>();
-            test.Add(new Test { Name = "Kyle", FavNumber = 3, Date = new DateTime(1986, 4, 11) });
-            test.Add(new Test { Name = "Sheila", FavNumber = 7, Date = new DateTime(1976, 7, 8) });
-            test.Add(new Test { Name = "Ashley", FavNumber = 14, Date = new DateTime(2002, 7, 3) });
-            test.Add(new Test { Name = "Carter", FavNumber = 1, Date = new DateTime(2016, 3, 1) });
-            test.Add(new Test { Name = "Moe", FavNumber = 12, Date = new DateTime(2009, 10, 27) });
 
-            //var rng = new Random();
-            //return Enumerable.Range(1, 5).Select(index => new Test
-            //{
-            //    Date = DateTime.Now.AddDays(index),
-            //    TemperatureC = rng.Next(-20, 55),
-            //    Summary = Summaries[rng.Next(Summaries.Length)]
-            //})
-            //.ToArray();
+            try
+            {
+                test.Add(new Test { Name = "Kyle", FavNumber = 3, Date = new DateTime(1986, 4, 11) });
+                test.Add(new Test { Name = "Sheila", FavNumber = 7, Date = new DateTime(1976, 7, 8) });
+                test.Add(new Test { Name = "Ashley", FavNumber = 14, Date = new DateTime(2002, 7, 3) });
+                test.Add(new Test { Name = "Carter", FavNumber = 1, Date = new DateTime(2016, 3, 1) });
+                test.Add(new Test { Name = "Moe", FavNumber = 12, Date = new DateTime(2009, 10, 27) });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
 
             return test;
+        }
+
+        [HttpGet("{id}")]
+        public Test Get(int id)
+        {
+            try
+            {
+                Test test = new Test { Name = "Kyle", FavNumber = 3, Date = new DateTime(1986, 4, 11) };
+                return test;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return new Test();
+            }
         }
     }
 }
